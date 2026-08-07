56519674699458043b6682e65989dab2b21862d6
Federico <federico.ortolani@student.unicam.it>
2026-08-07 14:46:17 +0200

    implementazione interfaccia per la gestione degli stati della pianta

--- src/main/java/it/unicam/cs/mpgc/rpg130722/modello/stato/StatoPianta.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130722/modello/stato/StatoPianta.java
@@ -1,4 +1,36 @@
 package it.unicam.cs.mpgc.rpg130722.modello.stato;
 
+import it.unicam.cs.mpgc.rpg130722.modello.entita.Pianta;
+
+/**
+ * Rappresenta lo stato di una pianta
+ * Definisce le azioni che possono essere eseguite su una pianta e che
+ * causano una transizione di stato.
+ */
 public interface StatoPianta {
+
+    /** Annaffia la pianta.
+     *
+     * @param pianta specifica la pianta da annaffiare
+     */
+    void annaffia(Pianta pianta);
+
+    /** Trascura la pianta.
+     *
+     * @param pianta specifica la pianta da trascurare
+     */
+    void trascura(Pianta pianta);
+
+    /** Purifica la pianta.
+     *
+     * @param pianta specifica la pianta da purificare
+     */
+    void purifica(Pianta pianta);
+
+    /**
+     * Restituisce una descrizione testuale dello stato.
+     * 
+     * @return la descrizione dello stato
+     */
+    String getDescrizione();
 }
