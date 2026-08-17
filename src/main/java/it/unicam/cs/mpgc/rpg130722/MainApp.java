dd9e4a72444abb9cafbfb57b3ba5ef476a916dcc
Federico <federico.ortolani@student.unicam.it>
2026-08-17 17:30:57 +0200

    implementazione MainApp

--- src/main/java/it/unicam/cs/mpgc/rpg130722/MainApp.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130722/MainApp.java
@@ -1,22 +1,46 @@
 package it.unicam.cs.mpgc.rpg130722;
 
+import it.unicam.cs.mpgc.rpg130722.modello.service.GiardinoService;
+import it.unicam.cs.mpgc.rpg130722.persistenza.FileGiardinoRepository;
+import it.unicam.cs.mpgc.rpg130722.persistenza.GiardinoRepository;
+import it.unicam.cs.mpgc.rpg130722.ui.controllers.GiardinoController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class MainApp extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("Radici RPG");
-        StackPane root = new StackPane(label);
-        Scene scene = new Scene(root, 640, 480);
+    public void start(Stage primaryStage) throws Exception
+    {
+        GiardinoRepository repository = new FileGiardinoRepository(Path.of("giardino.dat"));
+        GiardinoService service = new GiardinoService(repository);
 
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/it/unicam/cs/mpgc/rpg130722/ui/fxml/giardino-view.fxml")
+        );
+        Parent root = loader.load();
+
+        GiardinoController controller = loader.getController();
+        controller.impostaService(service);
+
+        Scene scene = new Scene(root, 640, 480);
         primaryStage.setTitle("Radici");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(event -> {
+            try {
+                service.salva();
+            }   catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     public static void main(String[] args) {
