bdcd95b8e52b827deefb0ce812e3ef05026eefa3
Federico <federico.ortolani@student.unicam.it>
2026-08-25 10:56:31 +0200

    giardino implementa Serializable

--- src/main/java/it/unicam/cs/mpgc/rpg130722/modello/entita/Giardino.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130722/modello/entita/Giardino.java
@@ -1,17 +1,16 @@
 package it.unicam.cs.mpgc.rpg130722.modello.entita;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class Giardino {
+public class Giardino implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private final List<Pianta> piante = new ArrayList<>();;
-
-    private List<Pianta> pianteCurate;
 
     public void aggiungiPianta(Pianta p) {
         piante.add(p);
@@ -26,16 +25,5 @@ public class Giardino {
     {
         for (Pianta p : piante)
             p.passaGiorno(r);
-    }
-
-    void setPianteCurate(Pianta p)
-    {
-        pianteCurate.add(p);
-        rimuoviPianta(p);
-    }
-
-    void rimuoviPianta(Pianta p)
-    {
-        this.piante.remove(p);
     }
 }
